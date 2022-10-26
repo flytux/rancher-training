3b0e7af99737ffa621c32e8977a60a9222840de5
Jaehoon <hooney.jung@gmail.com>
2022-10-26 13:37:32 +0900

    Update setup.sh

--- config/setup.sh
+++ config/setup.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
 
-echo "Update /etc/hosts file"
+echo "=== Update /etc/hosts file ==="
 cat >>/etc/hosts<<EOF
-192.0.212.1  vm01 rancher.vm01
-192.0.212.2  vm02 ldapadmin.vm02 tekton.vm02 argocd.vm02 grafana.vm02 
+10.136.0.3 vm01 rancher.vm01 ldapadmin.vm01 tekton.vm01 argocd.vm01
+10.136.0.2 vm02 
 EOF
 
-echo "Install Docker"
-zypper in -y docker zsh
-systemctl enable docker
-systemctl start docker
+echo "=== Install Docker ==="
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
 
-echo "Create k8sadm user"
+echo "=== Create k8sadm user ==="
 groupadd -g 2000 k8sadm
-useradd -m -u 2000 -g 2000 k8sadm
+useradd -m -u 2000 -g 2000 -s /bin/bash k8sadm
 echo -e "1\n1" | passwd k8sadm >/dev/null 2>&1
 echo ' k8sadm ALL=(ALL)   ALL' >> /etc/sudoers
 usermod -aG docker k8sadm
-ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa<<<y
+
+echo "=== Setup k8s tools ==="
+wget https://github.com/rancher/rke/releases/download/v1.3.15/rke_linux-amd64
+chmod 755 rke_linux-amd64 && sudo mv rke_linux-amd64 /usr/local/bin/rke
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod 755 kubectl && sudo mv kubectl /usr/local/bin
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+cat <<EOF >> ~/.bashrc
+# k8s alias
+
+source <(kubectl completion bash)
+complete -o default -F __start_kubectl k
+
+alias k=kubectl
+alias vi=vim
+alias kn='kubectl config set-context --current --namespace'
+alias kc='kubectl config use-context'
+alias kcg='kubectl config get-contexts'
+alias di='docker images --format "table {{.Repository}}:{{.Tag}}\t{{.ID}}\t{{.Size}}\t{{.CreatedSince}}"'
+EOF
