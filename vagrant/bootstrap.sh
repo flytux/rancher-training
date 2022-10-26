197268288cd9c780594f7d88d1fa7094d7f0d002
Jaehoon <hooney.jung@gmail.com>
2022-10-26 17:09:22 +0900

    Update bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -38,6 +38,28 @@ echo ' k8sadm ALL=(ALL)   ALL' >> /etc/sudoers
 
 usermod -aG docker k8sadm
 
+echo "Setup k8s tools"
+
+wget https://github.com/rancher/rke/releases/download/v1.3.15/rke_linux-amd64
+chmod 755 rke_linux-amd64 && sudo mv rke_linux-amd64 /usr/local/bin/rke
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod 755 kubectl && sudo mv kubectl /usr/local/bin
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+cat <<EOF >> ~/.bashrc
+# k8s alias
+
+source <(kubectl completion bash)
+complete -o default -F __start_kubectl k
+
+alias k=kubectl
+alias vi=vim
+alias kn='kubectl config set-context --current --namespace'
+alias kc='kubectl config use-context'
+alias kcg='kubectl config get-contexts'
+alias di='docker images --format "table {{.Repository}}:{{.Tag}}\t{{.ID}}\t{{.Size}}\t{{.CreatedSince}}"'
+EOF
+
 #sudo -u k8sadm -H sh -c "ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N \"\";sshpass -p 1 ssh-copy-id k8sadm@vm01;sshpass -p 1 ssh-copy-id k8sadm@vm02"
 
 
