b1ad6ceda767fe2485b4ff480661cd539daac5aa
Jaehoon <hooney.jung@gmail.com>
2022-10-26 13:56:43 +0900

    Update setup.sh

--- config/setup.sh
+++ config/setup.sh
@@ -24,17 +24,3 @@ chmod 755 rke_linux-amd64 && sudo mv rke_linux-amd64 /usr/local/bin/rke
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod 755 kubectl && sudo mv kubectl /usr/local/bin
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-
-cat <<EOF >> ~/.bashrc
-# k8s alias
-
-source <(kubectl completion bash)
-complete -o default -F __start_kubectl k
-
-alias k=kubectl
-alias vi=vim
-alias kn='kubectl config set-context --current --namespace'
-alias kc='kubectl config use-context'
-alias kcg='kubectl config get-contexts'
-alias di='docker images --format "table {{.Repository}}:{{.Tag}}\t{{.ID}}\t{{.Size}}\t{{.CreatedSince}}"'
-EOF
